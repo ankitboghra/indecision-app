7f75264481ef0ae5a4f58c5a1d171eea517d48d8
Ankitkumar Boghra <ankitboghra@gmail.com>
2019-12-31 10:50:01 +0530

    refactoring stateless components

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Action = (props) => {
-    return (
-        <div>
-            <button disabled={!props.hasOptions} onClick={props.pickOne}>What should I do?</button>
-        </div>
-    );
-}
+const Action = (props) => (
+    <div>
+        <button disabled={!props.hasOptions} onClick={props.pickOne}>What should I do?</button>
+    </div>
+);
 
 export default Action;
--- src/components/Options.js
+++ src/components/Options.js
@@ -2,19 +2,17 @@ import React from 'react';
 
 import Option from './Option';
 
-const Options = (props) => {
-    return (
-        <div>
-            <button onClick={props.handleRemoveAll} >Remove All</button>
-            {props.options.length === 0 && <p>Please enter an item to get started.</p>}
-            {props.options.map((option)=> (
-                <Option
-                    key={option}
-                    optionText={option}
-                    handleRemoveOption={props.handleRemoveOption}
-                />))}
-        </div>
-    );
-}
+const Options = (props) => (
+    <div>
+        <button onClick={props.handleRemoveAll} >Remove All</button>
+        {props.options.length === 0 && <p>Please enter an item to get started.</p>}
+        {props.options.map((option)=> (
+            <Option
+                key={option}
+                optionText={option}
+                handleRemoveOption={props.handleRemoveOption}
+            />))}
+    </div>
+);
 
 export default Options;
